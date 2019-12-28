5f595b9eefcda43fad0c88f9fc93d75132157f4a
desirekaleba <desirekaleba@gmail.com>
2019-12-28 06:28:14 +0300

    email validator updated and corrected

--- validation/register.js
+++ validation/register.js
@@ -19,7 +19,7 @@ module.exports = function validateRegisterInput(data){
     // Email checks
     if (Validator.isEmpty(data.email)){
         errors.email = "Email field is required";
-    }else if (Validator.isEmail(data.email)){
+    }else if (!Validator.isEmail(data.email)){
         errors.email = "Email is invalid";
     }
 
