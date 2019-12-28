a798cd1ddbbf01f5c1ab544427e4f375d226ba54
desirekaleba <desirekaleba@gmail.com>
2019-12-28 21:52:11 +0300

    landing modified

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -6,51 +6,79 @@ class Landing extends Component{
     render(){
         return (
             <div style={{
-                height: "75vh"
+                //height: "75vh"
+                marginLeft: "0",
+                paddingLeft: "0"
                 }}
-                className="container valign-wrapper">
-                    <div className="row">
-                        <div className="col s12 center-align">
-                            <h4>
-                                <b>A</b> register/login/auth app with
+                className="valign-wrapper" style={{
+                    //height: "75vh"
+                    marginLeft: "0",
+                    paddingLeft: "0"
+                    }}>
+                    <div className="row" style={{
+                //height: "75vh"
+                marginLeft: "0",
+                paddingLeft: "0"
+                }}>
+                        <div className="waves-effects waves-light"
+                            style={{
+                                width: "73%",
+                                float: "left",
+                                textAlign: "justify",
+                                fontWeight: "100",
+                                backgroundColor: "skyblue",
+                                color: "black",
+                                height: "100em",
+                                padding: "8rem 1rem"
+
+                                
+                            }}
+                        >
+                            <h5 style={{marginTop: "30vh"}}>
+                                A full-stack
                                 <span style={{
                                     fontFamily: "monospace"
                                     }}
-                                > MERN</span> stack
-                            </h4>
-                            <p className="flow-text grey-text text-darken-1">
-                                A full-stack app with user authentication via 
+                                > <b>MERN</b></span> stack app with user authentication via 
                                 passport and JWTs
-                            </p>
-                            <br/>
+                            </h5>
 
-                            <div className="col s6">
-                                <Link
-                                    to="/register"
-                                    style={{
-                                        width: "140px",
-                                        borderRaduis: "3px",
-                                        letterSpacing: "1.5px"
-                                    }}
-                                    className="btn btn-large waves-effects waves-light hoverable blue accent-3"
-                                >
-                                    Register
-                                </Link>
-                            </div>
+                        </div>
+                        <div className=""
+                        style={{
+                            width: "25%",
+                            float: "right",
+                            textAlign: "justify",
+                                fontWeight: "100",
+                                margin: "50vh 0 0 0",
+                                height: "75vh",
+                            
+                        }}>
+                                    <Link
+                                        to="/register"
+                                        style={{
+                                            width: "100%",
+                                            borderRadius: "3rem",
+                                            letterSpacing: "1.5px"
+                                        }}
+                                        className="btn btn-large waves-effects waves-light hoverable blue accent-3"
+                                    >
+                                        Register
+                                    </Link>
 
-                            <div className="col s6">
-                                <Link
-                                    to="/login"
-                                    style={{
-                                        width: "140px",
-                                        borderRadius: "3px",
-                                        letterSpacing: "1.5px"
-                                    }}
-                                className="btn btn-large btn-flat waves-effects white black-text"
-                                >
-                                    Log In
-                                </Link>
-                            </div>
+                                    <Link
+                                        to="/login"
+                                        style={{
+                                            width: "100%",
+                                            borderRadius: "3rem",
+                                            marginTop: "1em",
+                                            letterSpacing: "1.5px",
+                                            // display: "block"
+                                        }}
+                                    className="btn btn-large btn-flat waves-effects blue black-text"
+                                    >
+                                        Log In
+                                    </Link>
                         </div>
                     </div>
             </div>
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -5,18 +5,18 @@ class Navbar extends Component{
     render(){
         return(
             <div className="navbar-fixed">
-                <nav className="z-depth-0">
-                    <div className="nav-wrapper white">
+                <nav style={{height: "25vh", backgroundColor: "black"}} className="z-depth-0">
+                    <div className="black">
                         <Link
                             to="/"
                             style={
                                 {
-                                    fontFamily: "monospace"
+                                    fontFamily: "verdana"
                                 }
                             }
-                            className="col s5 brand-logo center black-text"
+                            className="col s5 brand-logo center white-text"
                         >
-                            <i className="material-icons">code</i>MERN example
+                            <h3>MERN</h3> Register-Login
                         </Link>
                     </div>
                 </nav>
